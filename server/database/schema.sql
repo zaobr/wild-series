be999664817e70ec989a78a227765f906354a922
Mehdi Ouhab <ouhab.me@gmail.com>
2024-06-03 16:11:15 +0200

    harmonia 05

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,8 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+CREATE table category (id int  PRIMARY KEY auto_increment not null, name VARCHAR(255) not NULL);
+
+create table program (id int PRIMARY KEY auto_increment not null, title varchar(255) not null, synopsis text not null, poster VARCHAR(255) not null, year INTEGER NOT NULL, category_id INTEGER not NULL, FOREIGN KEY (category_id) REFERENCES category(id));
+
